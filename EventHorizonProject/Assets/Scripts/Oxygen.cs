89ec3e8bac5ad7c318ab29fe68e301c180e13caa
Anthony <anth19800@hotmail.com>
2020-05-10 16:14:44 -0400

    Merge branch 'Leo' into Anthony

--- EventHorizonProject/Assets/Scripts/Oxygen.cs
+++ EventHorizonProject/Assets/Scripts/Oxygen.cs
@@ -5,13 +5,17 @@ using UnityEngine.UI;
 
 public class Oxygen : MonoBehaviour
 {
+    //controls 
+    public float jumpForce = 10f; //No pun intended
     ControllerInput inputActions;
+    bool aButtonPress = false;
+    public Slider oxygenBar;
 
     //Oxygen stuff
     [SerializeField] float maxOxygen;
     [SerializeField] float currentOxygen;
     public bool hasOxygen;
-    public Slider oxygenBar;
+    //public Slider oxygenBar;
     public float refillMultiplier = 5f;
     public float drainRate;
 
@@ -23,11 +27,28 @@ public class Oxygen : MonoBehaviour
     private float playerEntDistance;
     public float closenessCheck; //rename later i dunno what to call it lol
 
+    void Awake()
+    {
+        inputActions = new ControllerInput();
+        inputActions.PlayerControllerInput.aButton.started += AButton_started;
+        inputActions.PlayerControllerInput.aButton.canceled += AButton_canceled;
+
+    }
+
+    private void AButton_canceled(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        aButtonPress = false;
+    }
+
+    private void AButton_started(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        aButtonPress = true;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        currentOxygen = maxOxygen;
-        DrainOxygen(maxOxygen/2.0f);//debug feature
+        currentOxygen = maxOxygen/2.0f;
     }
 
     // Update is called once per frame
@@ -46,6 +67,15 @@ public class Oxygen : MonoBehaviour
         }
         else
             mm.TurnOnActivateMaterial(false);
+
+        if (aButtonPress)
+        {
+            DrainOxygen(drainRate);
+            if (hasOxygen)
+            {
+                playerEnt1.GetComponent<Rigidbody>().AddForce(new Vector3(0f, jumpForce, 0f));
+            }
+        }
 
         oxygenBar.value = OxygenBarValue();
     }
@@ -67,6 +97,8 @@ public class Oxygen : MonoBehaviour
         }
         else
         {
+            hasOxygen = false;
+            print("dead boi");
             //death n stuff
         }
     }
@@ -75,5 +107,15 @@ public class Oxygen : MonoBehaviour
     {
         float calculatedAmount = Mathf.Clamp((currentOxygen / maxOxygen), 0, 1);
         return calculatedAmount;
+    }
+    //Controller related
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
     }
 }
